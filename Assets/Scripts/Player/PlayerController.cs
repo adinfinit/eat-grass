fd6d83519eabce163e9d679b1a00e97f9d7822ea
Hendrik Eerikson <hendrik@eerikson.ee>
2019-04-27 11:52:00 +0300

    Added an initial player movement controller script.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    CharacterController characterController;
+
+    public float speed = 6.0f;
+    private Vector3 moveDirection = Vector3.zero;
+    private Quaternion cameraDirection;
+
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
+        cameraDirection = Quaternion.Euler(0, 45, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        moveDirection = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+        characterController.Move((cameraDirection * moveDirection) * Time.deltaTime * speed);
     }
 }
