98a5cd64244809ba9e5dafd1cc23cab177d70353
Hendrik Eerikson <hendrik@eerikson.ee>
2019-04-27 13:59:41 +0300

    Meta files and Weapons.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     CharacterController characterController;
-
+    
     public float speed = 6.0f;
     private Vector3 moveDirection = Vector3.zero;
     private Quaternion cameraDirection;
+    private Component[] weaponControllers;
 
     void Start()
     {
         characterController = GetComponent<CharacterController>();
         cameraDirection = Quaternion.Euler(0, 45, 0);
+
+        weaponControllers = GetComponentsInChildren<WeaponController>();
+        print(weaponControllers[0]);
     }
 
     // Update is called once per frame
@@ -24,5 +28,13 @@ public class PlayerController : MonoBehaviour
 
         moveDirection = new Vector3 (moveHorizontal, 0.0f, moveVertical);
         characterController.Move((cameraDirection * moveDirection) * Time.deltaTime * speed);
+
+        // Attack animation
+        if (Input.GetKeyDown (KeyCode.Space)) { 
+            print("attack!");
+            foreach (WeaponController wc in weaponControllers) {
+                wc.StartAttack();
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Player/WeaponController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    private float targetRotation;
+    private float attackTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRotation = transform.localEulerAngles.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        attackTimer += 10 * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(0, Mathf.Lerp(0, targetRotation, attackTimer), 0);
+    }
+
+    public void StartAttack()
+    {
+        print("Hyaaa");
+        print(targetRotation);
+        attackTimer = 0;
+        transform.eulerAngles = Vector3.zero;
+        targetRotation = 360f;
+    }
+}
