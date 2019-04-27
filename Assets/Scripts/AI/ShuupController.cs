0a0c6e57b7a079b1f925b940d910a1fd81b1c63d
Kaari Laanemäe <kaari@Kaaris-MacBook-Pro-2.local>
2019-04-27 13:49:20 +0300

    Sheep controller

--- Assets/Scripts/AI/ShuupController.cs
+++ Assets/Scripts/AI/ShuupController.cs
@@ -4,15 +4,75 @@ using UnityEngine;
 
 public class ShuupController : MonoBehaviour
 {
+    public GameObject player;
+    public float speed = 2.0f;
+    public float wanderSpeed = 0.5f;
+    public bool trigger;
+    public Vector3 randomDirection;
+    private Rigidbody rb;
+    private SphereCollider triggerCollider;
+    private float newDirectionCountdown = 0.0f;
+    private bool randomStanding = false;
+    private Vector3 chargePosition; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody>();
+
+        triggerCollider = gameObject.GetComponent<SphereCollider>();
+
+        if((player.transform.position - transform.position).magnitude < triggerCollider.radius)
+        {
+            trigger = true;
+        }
+        else { trigger = false; }
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() {
+        if (trigger)
+        {
+
+            chargePosition = player.transform.position;
+
+            Vector3 chargeDirection = (chargePosition - transform.position).normalized;
+
+            Debug.Log(chargeDirection);
+
+            rb.velocity = chargeDirection * speed;
+        }
+        else
+        {   
+            if (newDirectionCountdown <= 0.0f)
+            {
+                randomDirection = (new Vector3(Random.Range(-1.0f, 1.0f), 0.0f, Random.Range(-1.0f, 1.0f))).normalized;
+                newDirectionCountdown = Random.Range(50.0f, 100.0f);
+                randomStanding = (Random.value > 0.5f);
+            }
+            if (randomStanding == false)
+            {
+                transform.position += randomDirection * wanderSpeed * Time.deltaTime;
+            }
+
+            newDirectionCountdown -= 1.0f ;
+
+        }
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject == player) {
+            trigger = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            trigger = false;
+        }
     }
 }
