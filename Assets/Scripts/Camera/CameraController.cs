e08a3c962f1074acb3df663c00895faae77b7afc
Egon Elbre <egonelbre@gmail.com>
2019-04-27 11:47:25 +0300

    Add basic camera

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject target = null;
+
+    public float distance = 10f;
+
+    public float smoothTime = 0.3F;
+    private Vector3 velocity = Vector3.zero;
+
+    const float pitch = 20;
+    const float yaw = 45;
+
     void Start()
     {
-        
+        if (target == null)
+            target = GameObject.FindGameObjectWithTag("Player");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (target == null) return;
+
+        var offset = Quaternion.Euler(pitch, yaw, 0) * new Vector3(0, 0, distance);
+        var targetPosition = target.transform.position - offset;
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
 }
---
+++ Assets/Scripts/Misc/TestMovementController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMovementController : MonoBehaviour
+{
+    public float maxSpeed = 10.0f;
+    public Vector3 direction = Vector3.zero;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (rb == null) return;
+
+        var dx = Input.GetAxis("Horizontal");
+        var dy = Input.GetAxis("Vertical");
+
+        direction = new Vector3(dx, 0, dy);
+        if (direction.magnitude > 1.0f) direction.Normalize();
+    }
+
+    void FixedUpdate()
+    {
+        // rb.AddForce(direction * maxSpeed);
+        rb.velocity = direction * maxSpeed;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, transform.position + direction);
+    }
+}
