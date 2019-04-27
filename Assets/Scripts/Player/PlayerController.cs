9d5d8b18bd6419d498de6ed8f0a397c44041a5d9
Hendrik Eerikson <hendrik@eerikson.ee>
2019-04-27 17:26:35 +0300

    Dashing.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,7 +6,15 @@ public class PlayerController : MonoBehaviour
 {
     CharacterController characterController;
     
-    public float speed = 6.0f;
+    public float speed = 10.0f;
+    public float dashMultiplier = 3.0f;
+    public float dashDuration = 0.10f;
+    public float doubleTapCooldown = 0.3f;
+    private float doubleTapTimer = 0f;
+    private float dashTimer = 0f;
+    private bool currentlyDashing = false;
+    private Vector3 dashDir =Vector3.zero;
+    private KeyCode dashKey = KeyCode.Space;
     private Vector3 moveDirection = Vector3.zero;
     private Quaternion cameraDirection;
     private Component[] weaponControllers;
@@ -17,24 +25,93 @@ public class PlayerController : MonoBehaviour
         cameraDirection = Quaternion.Euler(0, 45, 0);
 
         weaponControllers = GetComponentsInChildren<WeaponController>();
-        print(weaponControllers[0]);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (DoubleTap()) {
+            currentlyDashing = true;
+            print("DASH!");
+
+        }
+
         float moveHorizontal = Input.GetAxis ("Horizontal");
         float moveVertical = Input.GetAxis ("Vertical");
 
         moveDirection = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        if (currentlyDashing) {
+            dashTimer += Time.deltaTime;
+            if (dashTimer >= dashDuration) {
+                dashTimer = 0f;
+                currentlyDashing = false;
+            }
+
+            moveDirection = dashMultiplier * dashDir;
+        }
+
         characterController.Move((cameraDirection * moveDirection) * Time.deltaTime * speed);
 
         // Attack animation
         if (Input.GetKeyDown (KeyCode.Space)) { 
-            print("attack!");
             foreach (WeaponController wc in weaponControllers) {
                 wc.StartAttack();
             }
         }
     }
+
+    bool DoubleTap() {
+        if (Input.GetKeyDown(KeyCode.W)) {
+            if (doubleTapTimer > 0 && doubleTapTimer < doubleTapCooldown && dashKey == KeyCode.W){
+                doubleTapTimer = 0;
+                dashDir = new Vector3(0, 0, 1);
+                return true;
+            } else {
+                doubleTapTimer = 0;
+                doubleTapTimer += Time.deltaTime;
+                dashKey = KeyCode.W;
+            }
+        } 
+        else if (Input.GetKeyDown(KeyCode.S)) {
+            if (doubleTapTimer > 0 && doubleTapTimer < doubleTapCooldown && dashKey == KeyCode.S){
+                doubleTapTimer = 0;
+                dashDir = new Vector3(0, 0, -1);
+                return true;
+            } else {
+                doubleTapTimer = 0;
+                doubleTapTimer += Time.deltaTime;
+                dashKey = KeyCode.S;
+            }
+        } 
+        if (Input.GetKeyDown(KeyCode.D)) {
+            if (doubleTapTimer > 0 && doubleTapTimer < doubleTapCooldown && dashKey == KeyCode.D){
+                doubleTapTimer = 0;
+                dashDir = new Vector3(1, 0, 0);
+                return true;
+            } else {
+                doubleTapTimer = 0;
+                doubleTapTimer += Time.deltaTime;
+                dashKey = KeyCode.D;
+            }
+        } if (Input.GetKeyDown(KeyCode.A)) {
+            if (doubleTapTimer > 0 && doubleTapTimer < doubleTapCooldown && dashKey == KeyCode.A){
+                doubleTapTimer = 0;
+                dashDir = new Vector3(-1, 0, 0);
+                return true;
+            } else {
+                doubleTapTimer = 0;
+                doubleTapTimer += Time.deltaTime;
+                dashKey = KeyCode.A;
+            }
+        } 
+        else {
+            if (doubleTapCooldown > 0) {
+                doubleTapTimer += Time.deltaTime;
+            }
+        }
+
+        return false;
+    }
+
 }
