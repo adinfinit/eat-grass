d4979660fec6a9bca4f393ebe8fa9457d49c3fd3
Egon Elbre <egonelbre@gmail.com>
2019-04-27 14:30:10 +0300

    Add stupid billboard rotator.

---
+++ Assets/Scripts/Camera/BillboardRotator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class BillboardRotator : MonoBehaviour
+{
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(CameraController.Pitch, CameraController.Yaw, 0);
+    }
+}
