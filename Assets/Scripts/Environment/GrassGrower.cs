0c26824aa0aba2416191f48a212a5ea24cd4ebb8
Egon Elbre <egonelbre@gmail.com>
2019-04-28 10:58:46 +0300

    Add automatic grass growing, create terrain prefab

---
+++ Assets/Scripts/Environment/GrassGrower.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(GrassController))]
+public class GrassGrower : MonoBehaviour
+{
+    public float Interval = 3.0f;
+    private float countdown = 0.0f;
+
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown > 0) return;
+        countdown = Interval;
+
+        var controller = GetComponent<GrassController>();
+        controller.Grow();
+    }
+}
