925e2442cbef51674e5d8e76cc595dc3f28c8aa4
Egon Elbre <egonelbre@gmail.com>
2019-04-28 19:28:11 +0300

    Add environment prefabs

---
+++ Assets/Scripts/Environment/RandomFlip.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomFlip : MonoBehaviour
+{
+    void Start()
+    {
+        var renderer = GetComponent<SpriteRenderer>();
+        renderer.flipX = Random.Range(0.0f, 1.0f) < 0.5f;
+        enabled = false;
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     CharacterController characterController;
-    
+
     public float speed = 10.0f;
     public float dashMultiplier = 3.0f;
     public float dashDuration = 0.10f;
@@ -13,7 +13,7 @@ public class PlayerController : MonoBehaviour
     private float doubleTapTimer = 0f;
     private float dashTimer = 0f;
     private bool currentlyDashing = false;
-    private Vector3 dashDir =Vector3.zero;
+    private Vector3 dashDir = Vector3.zero;
     private KeyCode dashKey = KeyCode.Space;
     private Vector3 moveDirection = Vector3.zero;
     private Quaternion cameraDirection;
@@ -22,7 +22,7 @@ public class PlayerController : MonoBehaviour
     public float MaxHealth = 200f;
     public float CurrentHealth = 200f;
     public float InvulnerableTime = 0.5f;  // time between taking damage
-    private float InvulnerableTimer= 0f;
+    private float InvulnerableTimer = 0f;
     private HealthBar healtbar;
 
     void Start()
@@ -37,19 +37,22 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (DoubleTap()) {
+        if (DoubleTap())
+        {
             currentlyDashing = true;
             print("DASH!");
         }
 
-        float moveHorizontal = Input.GetAxis ("Horizontal");
-        float moveVertical = Input.GetAxis ("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
-        moveDirection = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+        moveDirection = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        if (currentlyDashing) {
+        if (currentlyDashing)
+        {
             dashTimer += Time.deltaTime;
-            if (dashTimer >= dashDuration) {
+            if (dashTimer >= dashDuration)
+            {
                 dashTimer = 0f;
                 currentlyDashing = false;
             }
@@ -57,7 +60,7 @@ public class PlayerController : MonoBehaviour
             moveDirection = dashMultiplier * dashDir;
         }
 
-        characterController.Move( (cameraDirection * moveDirection) * Time.deltaTime * speed);
+        characterController.Move((cameraDirection * moveDirection) * Time.deltaTime * speed);
 
         /*
         RaycastHit hit;
@@ -74,8 +77,10 @@ public class PlayerController : MonoBehaviour
 
 
         // Attack animation
-        if (Input.GetKeyDown (KeyCode.Space)) { 
-            foreach (WeaponController wc in weaponControllers) {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            foreach (WeaponController wc in weaponControllers)
+            {
                 wc.StartAreaSlash();
             }
         }
@@ -90,59 +95,79 @@ public class PlayerController : MonoBehaviour
         }
         */
 
-        healtbar.transform.localScale = new Vector3(2 * (Mathf.Clamp(CurrentHealth, 0f, MaxHealth) / MaxHealth), 0.2f, 0f);
+        // healtbar.transform.localScale = new Vector3(2 * (Mathf.Clamp(CurrentHealth, 0f, MaxHealth) / MaxHealth), 0.2f, 0f);
 
         // constrain elevation
-        transform.position = new Vector3 (transform.position.x, 1f, transform.position.z);
+        transform.position = new Vector3(transform.position.x, 1f, transform.position.z);
         InvulnerableTimer += Time.deltaTime;
     }
 
-    bool DoubleTap() {
-        if (Input.GetKeyDown(KeyCode.W)) {
-            if (doubleTapTimer > 0 && doubleTapTimer < doubleTapCooldown && dashKey == KeyCode.W){
+    bool DoubleTap()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (doubleTapTimer > 0 && doubleTapTimer < doubleTapCooldown && dashKey == KeyCode.W)
+            {
                 doubleTapTimer = 0;
                 dashDir = new Vector3(0, 0, 1);
                 return true;
-            } else {
+            }
+            else
+            {
                 doubleTapTimer = 0;
                 doubleTapTimer += Time.deltaTime;
                 dashKey = KeyCode.W;
             }
-        } 
-        else if (Input.GetKeyDown(KeyCode.S)) {
-            if (doubleTapTimer > 0 && doubleTapTimer < doubleTapCooldown && dashKey == KeyCode.S){
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (doubleTapTimer > 0 && doubleTapTimer < doubleTapCooldown && dashKey == KeyCode.S)
+            {
                 doubleTapTimer = 0;
                 dashDir = new Vector3(0, 0, -1);
                 return true;
-            } else {
+            }
+            else
+            {
                 doubleTapTimer = 0;
                 doubleTapTimer += Time.deltaTime;
                 dashKey = KeyCode.S;
             }
-        } 
-        if (Input.GetKeyDown(KeyCode.D)) {
-            if (doubleTapTimer > 0 && doubleTapTimer < doubleTapCooldown && dashKey == KeyCode.D){
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (doubleTapTimer > 0 && doubleTapTimer < doubleTapCooldown && dashKey == KeyCode.D)
+            {
                 doubleTapTimer = 0;
                 dashDir = new Vector3(1, 0, 0);
                 return true;
-            } else {
+            }
+            else
+            {
                 doubleTapTimer = 0;
                 doubleTapTimer += Time.deltaTime;
                 dashKey = KeyCode.D;
             }
-        } if (Input.GetKeyDown(KeyCode.A)) {
-            if (doubleTapTimer > 0 && doubleTapTimer < doubleTapCooldown && dashKey == KeyCode.A){
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (doubleTapTimer > 0 && doubleTapTimer < doubleTapCooldown && dashKey == KeyCode.A)
+            {
                 doubleTapTimer = 0;
                 dashDir = new Vector3(-1, 0, 0);
                 return true;
-            } else {
+            }
+            else
+            {
                 doubleTapTimer = 0;
                 doubleTapTimer += Time.deltaTime;
                 dashKey = KeyCode.A;
             }
-        } 
-        else {
-            if (doubleTapCooldown > 0) {
+        }
+        else
+        {
+            if (doubleTapCooldown > 0)
+            {
                 doubleTapTimer += Time.deltaTime;
             }
         }
@@ -150,9 +175,11 @@ public class PlayerController : MonoBehaviour
         return false;
     }
 
-    public void getAttacked() {
+    public void getAttacked()
+    {
         print("Got hit!");
-        if (InvulnerableTimer > InvulnerableTime) {
+        if (InvulnerableTimer > InvulnerableTime)
+        {
             InvulnerableTimer = 0f;
             CurrentHealth -= 40;
             print("Health left " + CurrentHealth);
