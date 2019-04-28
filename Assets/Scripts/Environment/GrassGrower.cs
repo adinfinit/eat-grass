74903fc16e10264e3730d79a461082d14ed42f3e
Egon Elbre <egonelbre@gmail.com>
2019-04-28 15:03:41 +0300

    remove debug code

--- Assets/Scripts/Environment/GrassGrower.cs
+++ Assets/Scripts/Environment/GrassGrower.cs
@@ -6,11 +6,8 @@ using UnityEngine;
 public class GrassGrower : MonoBehaviour
 {
     public float Interval = 3f;
-    public float Radius = 10.0f;
 
     private float countdown = 0.0f;
-
-    private Vector3 lastPoint;
 
     void Update()
     {
@@ -20,29 +17,5 @@ public class GrassGrower : MonoBehaviour
 
         var controller = GetComponent<GrassController>();
         controller.GrowPatchy(0.5f);
-        return;
-
-        var terrain = GetComponent<Terrain>();
-        if (terrain == null)
-        {
-            Debug.LogWarning("Terrain component missing.", gameObject);
-            return;
-        }
-
-        var data = terrain.terrainData;
-        var point = new Vector3(
-            Random.Range(0f, data.size.x),
-            0,
-            Random.Range(0f, data.size.z)
-        );
-
-        controller.PlantPatchy(point + terrain.transform.position, Radius, 0.7f);
-
-        lastPoint = point + terrain.transform.position;
-    }
-
-    void OnDrawGizmos()
-    {
-        Gizmos.DrawSphere(lastPoint, Radius);
     }
 }
